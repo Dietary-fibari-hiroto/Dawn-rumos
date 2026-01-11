d58621f80961c0e66d6d3a5586680640548df917
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2026-01-11 21:04:29 +0900

    最新版コミット

--- Lumina/Dawn_Lumina_Sangria/src/DeviceMetaData.cpp
+++ Lumina/Dawn_Lumina_Sangria/src/DeviceMetaData.cpp
@@ -1,7 +1,7 @@
 #include "DeviceMetaData.h"
 
 //const char* DEVICE_NAME = "Astrolume_Sangria_1";
-const char* DEVICE_NAME = "Astrolume_Startrail_2";
+const char* DEVICE_NAME = "Astrolume_reframejar_1";
 const char* DEVICE_SERIES = "Sangria";
-const int CONST_NUM_LEDS = 71;//36
-const int CONST_LED_PIN = 32;
+const int CONST_NUM_LEDS = 16;//36
+const int CONST_LED_PIN = 21;
